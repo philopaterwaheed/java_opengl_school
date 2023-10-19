ec0e2a7cb8cdcbea3f3f131002559c416759b78f
Your Name <you@example.com>
2023-10-19 09:41:11 +0300

    commands

--- calcu/calculator.java
+++ calcu/calculator.java
@@ -1,28 +1,38 @@
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 public class calculator extends JFrame implements ActionListener {
-    TextField box = new TextField();
+   static TextField inbox = new TextField();
+   static TextField outbox = new TextField();
     pbutton buttons [] = new pbutton[35];
     JPanel jp = new JPanel();
+    static ArrayList <String> query = new ArrayList <String> ();
+    static String outquery = "";
+    static long output =0 ;
+
+
     public static void main(String[] args) {
             calculator app = new calculator();
     }
+
+
     public calculator() {
         super("calculator");
         setSize(500, 500);
 	setResizable(false);
-	this.setLocation(300,300);
+	setLocation(300,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-	box.setSize(300,400);
-	
-	int bunnum = 0;
+	inbox.setPreferredSize( new Dimension(50,50) );
+	int bunnum = 0; //buttons number 
+
 	for (int i = 0 ; i < 7; i ++ ) 
 	{
 		for (int c = 0; c < 5; c++) {
 			buttons[bunnum]= new pbutton(instructions.map[i][c]);
+			buttons[bunnum].func = (instructions.func[i][c]);
 			jp.add(buttons[bunnum].button);
 			buttons[bunnum].button.addActionListener(this);
 			bunnum ++;
@@ -31,19 +41,43 @@ public class calculator extends JFrame implements ActionListener {
 
 	jp.setLayout(new GridLayout (7,5)); // the more not inculded 
         add(jp, BorderLayout.SOUTH);
-	add(box, BorderLayout.SOUTH);
+	add(inbox, BorderLayout.NORTH);
+	add(outbox, BorderLayout.CENTER);
         setVisible(true);
 	jp.setFocusable(false);
-	System.out.println("hello");
     }
+
+
     @Override
     public void actionPerformed(ActionEvent e){
 	    System.out.println("button clicked");
 	    if( e.getSource() instanceof JButton) {
-	      String out = ((JButton)e.getSource()).getText();
-	       System.out.println(out);
+	      // String out = ((JButton)e.getSource()).getText();
+	       // System.out.println(out);
+	       pbutton gotButton = getButton((JButton)e.getSource());
+	       gotButton.prosess(); // enter the function to prosees the button in any way possible
+	       String out = ""; 
+	       for (String c : query) //gathering info from the query
+		       out+= c;
+	       inbox.setText(out);
+	       outbox.setText(outquery);
+	
     
     	}
     }
+    
+    pbutton getButton(JButton input) { 
+	for (pbutton x : buttons)
+		if (x.button.equals(input))
+			return x ; 
+	return null ;
+    }
+    
+	void clac (ArrayList query) {
+		
+		ArrayList <ArrayList <String> >  
+
+	}
+    
 
 }
--- calcu/pbutton.java
+++ calcu/pbutton.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -5,11 +6,58 @@ public class pbutton{
 	String name ; 
 	int val;
 	JButton button = new JButton() ; 
+	String intoInputBox ;
+	int func ; 
+
+
 	pbutton(String name ) {
 		this.name = name ;
 		this.button = new JButton(name);
 	}
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(this.button.equals((JButton)obj));
+	}
+	/*
+	 * execution of a button when pressed ;
+	 *
+	 */
+	 void exe (int code) {
+		 switch (code)
+		 {
+			case 69 : // c , ce
+				 calculator.query.clear();
+				 break ; 
+			case 99 : //  = 
+				  //
+				 if (calculator.query != null){
+					calculator.outquery =  calculator.query.toString();
+					 calculator.query.clear();
+				 }
+
+				 break;
+			case -1 :
+				 if (!calculator.query.isEmpty())
+				 calculator.query.remove(calculator.query.size()-1);
+				 break;
+			case -2 :
+				 break; 
+		}
+	 }
+
+
+	 void prosess ()
+	 {
+		 if (this.func != 0 )
+			 exe (this.func) ;
+		 else 
+			 calculator.query.add(this.name);
+	 }
+	 
 }
+/**
+ * 
+ */
 class instructions {
 	 static String map [][] = {
 		 {   "x^3" ,"y^/x" ,"sin-1" ,"cos-1" , "tan-1"} , 
@@ -20,5 +68,16 @@ class instructions {
 		 {   "+-" ,"1" ,"2" ,"3" , "+"} , 
 		 {   "(" ,")" ,"0" ,"." , "="} , 
 	 } ;  
+	 static int func [][] = {
+		 {1 ,2 , 3, 4 ,5 } , 
+		 {6 ,7 , 8, 9 ,10 } , 
+		 {11 ,69 , 69, -1 , 0 } , 
+		 {0, 0, 0, 0 , 0 } , 
+		 {18 , 0, 0, 0 ,0  } , 
+		 {-2,  0, 0, 0 ,0 } , 
+		 {0, 0 , 0, 25 ,99 } 
+		 // 0 are just numbers so no funtion
+	 } ;  
+	 
 	private instructions(){}
 }
